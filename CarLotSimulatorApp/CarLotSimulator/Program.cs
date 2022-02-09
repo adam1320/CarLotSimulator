812eebd049f5d71db956130bd5bf4a00125d07b9
Adam Brannon <adam1320@rocketmail.com>
2022-02-09 14:41:03 -0500

    Completed assignment

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,8 +6,39 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
-            //TODO
+            CarLot lot = new CarLot();
 
+            var car1 = new Car();
+            car1.Year = 2013;
+            car1.Make = "Dodge";
+            car1.Model = "Challenger";
+            car1.EngineNoise = "Rumble, rumble";
+            car1.HonkNoise = "Beep, beep";
+            car1.IsDriveable = true;
+            car1.MakeEngineNoise();
+            car1.MakeHonkNoise();
+
+            lot.CarList.Add(car1);
+
+            var car2 = new Car(2014, "Ram", "2500", "Clatter", "TrainHorn", true);
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+
+            lot.CarList.Add(car2);
+
+
+            var car3 = new Car() { Year = 2019, Make = "KIA", Model = "Sorento", EngineNoise = "Purr", HonkNoise = "Beep", IsDriveable = true };
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
+
+            lot.CarList.Add(car3);
+
+            foreach (var car in lot.CarList)
+                Console.WriteLine($"{car.Year}, {car.Make}, {car.Model}, {car.EngineNoise}, {car.HonkNoise}");
+
+
+
+            //TODO
             //Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
@@ -21,6 +52,15 @@ namespace CarLotSimulator
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            //See above for completion of Bonus 1!
+            //---------------------------------------
+
+
+
+
+
+
 
             //*************BONUS X 2*************//
 
